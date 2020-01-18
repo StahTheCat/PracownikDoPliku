f4fef4fbbf7f49a4f74d9a1bc0c2e58d30a86b67
Stah <stahthecat@gmail.com>
2020-01-18 23:15:40 +0100

    działą!

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -13,45 +13,54 @@ import java.util.Scanner;
 
 public class App {
 
-    public static File fileCreator(Firma org, Pracownik mruk) throws IOException {
+    public static void fileCreator(Firma org, List<Pracownik> lista) throws IOException {
 
+        //zawartość pliku
+        StringBuilder sb = new StringBuilder();
+        sb.append("Firma: "+ org.companyName +"\n");
+        sb.append("Szef: "+ org.bossName + "; " + lista.get(0).wynagrodzenie+" kocimiętki"+"\n");
+        sb.append("Pracownicy: "+ org.teamMatesNumber + "\n");
 
+        for (int i = 1; i< lista.size();i++){
+            //dla psa kość
+            if (lista.get(i).getWynagrodzenie()<10){
+                sb.append(lista.get(i).getImieNazwisko()+" "+lista.get(i).getWynagrodzenie()+" kość"+"\n");
+            } else {
+                sb.append(lista.get(i).getImieNazwisko() + " " + lista.get(i).getWynagrodzenie() + " kocimiętki" + "\n");
+            }
+        }
+
+        //tworzymy folder
         Path folder = Paths.get("src", "main", "java", org.companyName);
         if (!Files.exists(folder)) {
             Files.createDirectories(folder);
         }
 
-        Path plik = folder.resolve(org.companyName+".txt");
-        if (Files.exists(plik)){
+        //tworzymy plik
+        Path plik = folder.resolve(org.companyName + ".txt");
+        if (!Files.exists(plik)) {
             Files.createFile(plik);
-           // Files.write()
+            Files.writeString(plik, sb);
         }
 
+    } //koniec metody
 
 
-//        String miesiac = String.format("%02d", mm);
-//        Path folder = Paths.get("testMiesiace", yy + "-" + miesiac);
-//        if (!Files.exists(folder)) {
-//            Files.createDirectories(folder);
-//        }
-//        Path plikKalendarz = folder.resolve(yy + "-" + miesiac + ".txt");
-//        if (!Files.exists(plikKalendarz)) {
-//            Files.createFile(plikKalendarz);
-//            Files.writeString(plikKalendarz, kalendarz(mm,yy));
+    public static void main(String[] args) throws IOException {
+        Pracownik klebuszek = new Pracownik("Bardzo Ważny Kot", 40000);
+        Pracownik mruczek = new Pracownik("Pan Kłębuszek", 30000);
+        Pracownik pazur = new Pracownik("Lord Pazur", 35000);
 
-    }
-//    Firma <Nazwa>
-////    Szef: <Imię Nazwisko>; <Wynagrodzenie>;
-////    Pracownicy: <Liczba>
-////<Imię Nazwisko>; <Wynagrodzenie>;
-////<Imię Nazwisko>; <Wynagrodzenie>;
-
-
-    public static void main(String[] args) {
-        Firma catcave = new Firma("Cat Cave Corporation", "The Grand Catze", 3);
-        Pracownik klebuszek = new Pracownik("Pan Kłębuszek", 40000);
-        Pracownik mruczek = new Pracownik("Gemeral Mruczek", 30000);
         Pracownik kajtek = new Pracownik("Pies Kajtek", 1);
+
+        List<Pracownik> lista = new ArrayList<>();
+        lista.add(klebuszek);
+        lista.add(mruczek);
+        lista.add(pazur);
+        lista.add(kajtek);
+        Firma catcave = new Firma("Cat Cave Corporation", "The Grand Catze", lista.size());
+
+        fileCreator(catcave, lista);
 
     } //koniec maina
 }//koniec klasy
